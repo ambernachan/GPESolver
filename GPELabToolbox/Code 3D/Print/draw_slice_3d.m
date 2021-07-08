15bd4511a706717376718e0fb3e18c31f91e8371
Amber de Bruijn <amberdebruijn@gmail.com>
2021-07-08 14:51:43 +0200

    small error fixed

--- GPELabToolbox/Code 3D/Print/draw_slice_3d.m
+++ GPELabToolbox/Code 3D/Print/draw_slice_3d.m
@@ -6,13 +6,14 @@
 %%          Figure: Structure containing variables concerning the figures (structure)
 
 function draw_slice_3d(phi,Geometry3D,Figure)
-figure(Figure.label); % Number of the figure
-clf(Figure.label); % Clear the figure
-slice(Geometry3D.X,Geometry3D.Y,Geometry3D.Z,phi,Figure.Sy,Figure.Sx,Figure.Sz); % Computing the slice
-shading interp; % Setting shading
-colormap(Figure.map); % Setting the colormap
-colorbar; % Adding the colorbar
-xlabel(Figure.x); % Setting x-axis label
-ylabel(Figure.y); % Setting y-axis label
-title(Figure.title); % Setting title of the figure
-drawnow;
+    figure(Figure.label); % Number of the figure
+    clf(Figure.label); % Clear the figure
+    slice(Geometry3D.X,Geometry3D.Y,Geometry3D.Z,phi,Figure.Sy,Figure.Sx,Figure.Sz); % Computing the slice
+    shading interp; % Setting shading
+    colormap(Figure.map); % Setting the colormap
+    colorbar; % Adding the colorbar
+    xlabel(Figure.x); % Setting x-axis label
+    ylabel(Figure.y); % Setting y-axis label
+    title(Figure.title); % Setting title of the figure
+    drawnow;
+end
